542275f2d3c667e5461b73aaa90388dbabfae4f8
Shaheer cp <shaheercp890@gmail.com>
2025-01-27 12:16:06 +0530

    Binary search tree insert,search

---
+++ Tree/binaryST.js
@@ -0,0 +1,59 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+    isEmpty(){
+        return this.root === null;
+    }
+    insert(value){
+        const newNode = new Node(value);
+        if(this.isEmpty()){
+            this.root = newNode;
+        }else{
+            this.insertNode(this.root, newNode)
+        }
+    }
+    insertNode(root, newNode){
+        if(newNode.value<root.value){
+            if(root.left === null){
+                root.left = newNode;
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right == null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+    search(root,value){
+        if(!root){
+            return false;
+        }else{
+            if(root.value===value){
+                return true;
+            }else if(root.value>value){
+                return this.search(root.left, value);
+            }else{
+                return this.search(root.right, value);
+            }
+        }
+    }
+}
+
+const bst = new BinarySearchTree();
+console.log('Tree is empty ?',bst.isEmpty())
+bst.insert(10);
+bst.insert(4);
+bst.insert(15);
+console.log(bst.search(bst.root,11))
+console.log(bst.search(bst.root,15))
