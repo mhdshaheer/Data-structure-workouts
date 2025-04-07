f761944918df93cf054989259adaaeb3f5749d54
Shaheer cp <shaheercp890@gmail.com>
2025-04-07 16:47:03 +0530

    Rehashing in Hashtable

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -2,6 +2,8 @@ class HashTable{
     constructor(size){
         this.table = new Array(size);
         this.size = size;
+        this.count = 0
+        this.loadFactor = 0.7;
     }
     hash(key){
         let total = 0;
@@ -15,13 +17,18 @@ class HashTable{
         let bucket = this.table[index];
         if(!bucket){
             this.table[index] = [[key,value]]
+            this.count++;
         }else{
             const samekeyExist = bucket.find(item=>item[0]===key);
             if(samekeyExist){
                 samekeyExist[1] = value;
             }else{
                 bucket.push([key,value])
+                this.count++
             }
+        }
+        if(this.count/this.size>this.loadFactor){
+            this.rehash();
         }
     }
     get(key){
@@ -41,6 +48,9 @@ class HashTable{
         let bucket = this.table[index];
         if(bucket){
             this.table[index] = bucket.filter(item=>item[0]!==key);
+            if(this.count>0){
+                this.count--
+            }
         }
     }
     display(){
@@ -50,11 +60,25 @@ class HashTable{
             }
         }
     }
+    rehash(){
+        console.log("Re hashing...")
+        let oldTable = this.table;
+        this.size = this.size*2;
+        this.table = new Array(this.size);
+        this.count = 0;
+        for(let bucket of oldTable){
+            if(bucket){
+                for(let [key,value] of bucket){
+                    this.set(key,value);
+                }
+            }
+        }
+    }
 }
-const table = new HashTable(30);
+const table = new HashTable(10);
 table.set('name','shaheer');
 table.set('mane','zero');
 table.set('age',22);
 table.set('place','mukkam');
-table.remove('mane')
+// table.remove('mane')
 table.display()
